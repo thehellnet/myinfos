828f79212833209d63b0d94b810153ed8045a970
sardylan <sardylan@gmail.com>
2017-02-16 15:46:36 +0100

    Code cleanup and GUI bugfix

--- app/src/main/java/org/thehellnet/mobile/myinfos/MyInfos.java
+++ app/src/main/java/org/thehellnet/mobile/myinfos/MyInfos.java
@@ -3,10 +3,20 @@ package org.thehellnet.mobile.myinfos;
 import android.app.Application;
 import android.content.Context;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_NETWORK_STATE;
+import static android.Manifest.permission.READ_PHONE_STATE;
+
 /**
  * Created by sardylan on 29/08/15.
  */
 public class MyInfos extends Application {
+
+    public static final String[] PERMISSIONS = new String[]{
+            ACCESS_NETWORK_STATE,
+            READ_PHONE_STATE,
+            ACCESS_COARSE_LOCATION
+    };
 
     private static Context applicationContext;
 
--- app/src/main/java/org/thehellnet/mobile/myinfos/activity/MainActivity.java
+++ app/src/main/java/org/thehellnet/mobile/myinfos/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
@@ -11,7 +12,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.telephony.CellInfo;
 import android.telephony.TelephonyManager;
-import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -27,22 +27,27 @@ import org.thehellnet.mobile.myinfos.utility.AppUtils;
 import java.util.List;
 import java.util.Locale;
 
-import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
-import static android.Manifest.permission.ACCESS_NETWORK_STATE;
-import static android.Manifest.permission.READ_PHONE_STATE;
+import static org.thehellnet.mobile.myinfos.MyInfos.PERMISSIONS;
 
 public class MainActivity extends Activity {
 
     private static final int REQUEST_CODE_READ_PHONE_STATE = 1;
 
-    private static final String[] PERMISSIONS = new String[]{
-            ACCESS_NETWORK_STATE,
-            READ_PHONE_STATE,
-            ACCESS_COARSE_LOCATION
-    };
-
     private TelephonyManager telephonyManager;
     private ConnectivityManager connectivityManager;
+
+    private EditText imei;
+    private EditText swver;
+    private EditText deviceId;
+    private EditText simIccid;
+    private EditText simCountry;
+    private EditText simOperator;
+    private EditText number;
+    private EditText operator;
+    private EditText subscriber;
+    private EditText networkCount;
+    private EditText networkType;
+    private EditText cellList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,6 +105,19 @@ public class MainActivity extends Activity {
     private void initPrivates() {
         telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
         connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+        imei = (EditText) findViewById(R.id.imei_value);
+        swver = (EditText) findViewById(R.id.swver_value);
+        deviceId = (EditText) findViewById(R.id.device_id_value);
+        simIccid = (EditText) findViewById(R.id.sim_iccid_value);
+        simCountry = (EditText) findViewById(R.id.sim_country_value);
+        simOperator = (EditText) findViewById(R.id.sim_operator_value);
+        number = (EditText) findViewById(R.id.number_value);
+        operator = (EditText) findViewById(R.id.operator_value);
+        subscriber = (EditText) findViewById(R.id.subscriber_value);
+        networkCount = (EditText) findViewById(R.id.network_count_value);
+        networkType = (EditText) findViewById(R.id.network_type_value);
+        cellList = (EditText) findViewById(R.id.network_celllist_value);
     }
 
     private boolean checkPermissions() {
@@ -125,60 +143,22 @@ public class MainActivity extends Activity {
             return;
         }
 
-        EditText imei = (EditText) findViewById(R.id.imei_value);
-        EditText swver = (EditText) findViewById(R.id.swver_value);
-        EditText deviceId = (EditText) findViewById(R.id.device_id_value);
-        EditText simIccid = (EditText) findViewById(R.id.sim_iccid_value);
-        EditText simCountry = (EditText) findViewById(R.id.sim_country_value);
-        EditText simOperator = (EditText) findViewById(R.id.sim_operator_value);
-        EditText number = (EditText) findViewById(R.id.number_value);
-        EditText operator = (EditText) findViewById(R.id.operator_value);
-        EditText subscriber = (EditText) findViewById(R.id.subscriber_value);
-        EditText networkCount = (EditText) findViewById(R.id.network_count_value);
-        EditText networkType = (EditText) findViewById(R.id.network_type_value);
-
-        if (telephonyManager.getDeviceId() != null) {
-            updateValue(imei, telephonyManager.getDeviceId());
-        }
-
-        if (telephonyManager.getDeviceSoftwareVersion() != null) {
-            updateValue(swver, telephonyManager.getDeviceSoftwareVersion());
-        }
-
+        updateValue(imei, telephonyManager.getDeviceId());
+        updateValue(swver, telephonyManager.getDeviceSoftwareVersion());
         updateValue(deviceId, Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID));
-
-        if (telephonyManager.getSimSerialNumber() != null) {
-            updateValue(simIccid, telephonyManager.getSimSerialNumber());
-        }
-
-        if (telephonyManager.getSimCountryIso() != null) {
-            updateValue(simCountry, telephonyManager.getSimCountryIso());
-        }
-
-        if (telephonyManager.getSimOperatorName() != null) {
-            updateValue(simOperator, telephonyManager.getSimOperatorName());
-        }
-
-        if (telephonyManager.getLine1Number() != null) {
-            updateValue(number, telephonyManager.getLine1Number());
-        }
-
-        if (telephonyManager.getNetworkOperatorName() != null) {
-            updateValue(operator, telephonyManager.getNetworkOperatorName());
-        }
-
-        if (telephonyManager.getSubscriberId() != null) {
-            updateValue(subscriber, telephonyManager.getSubscriberId());
-        }
+        updateValue(simIccid, telephonyManager.getSimSerialNumber());
+        updateValue(simCountry, telephonyManager.getSimCountryIso());
+        updateValue(simOperator, telephonyManager.getSimOperatorName());
+        updateValue(number, telephonyManager.getLine1Number());
+        updateValue(operator, telephonyManager.getNetworkOperatorName());
+        updateValue(subscriber, telephonyManager.getSubscriberId());
 
         if (telephonyManager.getAllCellInfo() != null) {
             updateValue(networkCount, String.valueOf(telephonyManager.getAllCellInfo().size()));
             updateCellInfo(telephonyManager.getAllCellInfo());
         }
 
-        if (connectivityManager.getActiveNetworkInfo() != null) {
-            updateValue(networkType, String.valueOf(connectivityManager.getActiveNetworkInfo().getSubtypeName()));
-        }
+        updateNetworkType(connectivityManager.getActiveNetworkInfo());
     }
 
     private void showVersionToast() {
@@ -202,13 +182,22 @@ public class MainActivity extends Activity {
             return;
         }
 
-        EditText cellList = (EditText) findViewById(R.id.network_celllist_value);
         cellList.setText("");
 
         for (int i = 0; i < cellInfos.size(); i++) {
             CellInfo cellInfo = cellInfos.get(i);
             CellData cellData = CellDataFactory.parseData(cellInfo);
             cellList.append(String.format(Locale.US, "%d. %s\n", i + 1, cellData.toString()));
+        }
+
+        cellList.setKeyListener(null);
+    }
+
+    private void updateNetworkType(NetworkInfo networkInfo) {
+        if (networkInfo.getSubtypeName() != null && networkInfo.getSubtypeName().length() > 0) {
+            updateValue(networkType, connectivityManager.getActiveNetworkInfo().getSubtypeName());
+        } else {
+            updateValue(networkType, connectivityManager.getActiveNetworkInfo().getTypeName());
         }
     }
 }
